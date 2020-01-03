762b36f3c3694daf8e01d8e2c91a934a344a550d
mmiah00 <mmiah00@stuy.edu>
2020-01-03 04:40:33 +0000

    fixing

--- write.c
+++ write.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <errno.h>
 #include <sys/sem.h>
 #include <sys/shm.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <fcntl.h>
 #include <unistd.h>
+
 
 #define KEY1 24601
 #define KEY2 24602
@@ -25,11 +26,11 @@ int main () {
     semop (sem, &buf, 1);
     int shm = shmget (KEY2, 100, 0);
     if (shm < 0) {
-      printf ("Error: %s", strerror (errno));
+      printf ("Shared Memory Error: %s\n", strerror (errno));
     }
     else {
       int fd = open ("file.txt", O_WRONLY);
-      char * last = shmat (shm, 0,0); 
+      char * last = shmat (shm, 0,0);
       printf ("Last addition: %s\n", last);
       char *next; ;
       fgets (next, 256, stdin);
